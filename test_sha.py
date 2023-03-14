f6fadc8f594cddc97263301fde85d1ccc6c7b03d
Justine LV <justine.lopesvicente@gmail.com>
2023-03-14 16:15:03 +0100

    fix error in test_sha.py

--- test_sha.py
+++ test_sha.py
@@ -6,10 +6,10 @@ def test_sha():
     algo = SHA()
     msg = "Message"
     key = "adtjcopr"
-
+    result = "4fb472dfc43def7a46ad442c58ac532f89e0c8a96f23b672f5fd637652"\
+    "eab158d4d589444ef7530a34e6626b40830b4e1ec5364611ae31c599bffa958e8b4c4e"
     encrypted = algo.encrypt(msg, key)
-    assert encrypted == "4fb472dfc43def7a46ad442c58ac532f89e0c8a96f23b672f5fd637652eab158d4d5894"
-    "44ef7530a34e6626b40830b4e1ec5364611ae31c599bffa958e8b4c4e"
+    assert encrypted == result
 
 
 if __name__ == '__main__':
